fcb0e12378ca959a22cde8f36d48bbe8d10e1091
yavnek <jonatan.gasiorowski@gmail.com>
2024-04-24 20:04:13 +0200

    implementacja rejestracji uzytkownika

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+# model profilu
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    u_id = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_img = models.ImageField(upload_to='profile_images', default = 'def_prof_pic.png')
+    fav_couisine = models.CharField(max_length=60, blank=True)
+
+    def __str__(self):
+        return  self.user.username
