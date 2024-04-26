18df4af0cf0ed0d073a9cf8a63d3f92d0bdc50b3
yavnek <jonatan.gasiorowski@gmail.com>
2024-04-26 22:45:21 +0200

    Implementacja Postow

--- core/views.py
+++ core/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'user_profile': user_profile, 'posts': posts})
 
 
 def singup(requset):
@@ -85,4 +89,17 @@ def settings(request):
         return redirect('settings')
     return render(request, 'settings.html', {'user_profile': user_profile})
 
+
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image = image, caption = caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
 
