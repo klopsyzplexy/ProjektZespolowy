e0122bb994e2e62da8b38fa91d9eb1ab661f7c69
klopsyzplexy <dominik.bednarski4@gmail.com>
2024-04-28 09:53:25 +0200

    profile

--- core/urls.py
+++ core/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signin', views.signin, name='signin'),
     path('logout', views.logout, name='logout'),
     path('settings', views.settings, name='settings'),
-    path('upload', views.upload, name='upload')
+    path('upload', views.upload, name='upload'),
+    path('profile/<str:pk>', views.profile, name='profile'),
 ]
